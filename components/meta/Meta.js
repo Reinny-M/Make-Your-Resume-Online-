3c66efdfede9bd0f9c0ff7f7ac0ec57201b58b0c
Reinhard Maroa Babere <reinhardbabere@gmail.com>
2026-03-12 12:38:57 +0300

    Update Meta.js

--- components/meta/Meta.js
+++ components/meta/Meta.js
@@ -1,7 +1,6 @@
 import Head from "next/head";
-
 export default function Meta({ title, keywords, description }) {
-    const homepage = "https://free-next-resume-maker.vercel.app";
+    const homepage = "https://reinny-m.github.io/Make-Your-Resume-Online-";
     const logo = "/assets/logo.png";
     const fevicon = "/assets/logo.png";
     function isiteJsonLd() {
@@ -13,30 +12,28 @@ export default function Meta({ title, keywords, description }) {
                 "logo": ${logo},
                 "contactPoint": {
                     "@type": "ContactPoint",
-                    "telephone": "+91 1234567890",
+                    "telephone": "+254798791235",
                     "contactType": "customer service"
                 },
                 "image": ${logo},
                 "description": ${description},
-                "founder": "HOT HEAD",
-                "foundingDate": "2024",
-                "foundingLocation": "IN",
-                "email": "xyz@gmail.com",
-                "telephone": "+91 1234567890",
-                "areaServed": "IN",
+                "founder": "Reinhard M. Babere",
+                "foundingDate": "2026",
+                "foundingLocation": "KE",
+                "email": "reinhardbabere@gmail.com",
+                "telephone": "+254798791235",
+                "areaServed": "Worldwide",
                 "keywords": ${keywords},
                 "mainEntityOfPage": ${homepage},
                 "knowsAbout": ${keywords},
                 "knowsLanguage": "English",
-                "memberOf": "Oensource",
-                "owns": "HOT HEAD",
+                "memberOf": "Opensource",
+                "owns": "Reinhard M. Babere",
                 "publishingPrinciples": ${homepage},
-                "slogan": "free resume maker for humanity"
+                "slogan": "Free resume maker for job seekers worldwide"
             }`
         }
     }
-
-
     return (
         <Head>
             <meta name="viewport" content="width=device-width, initial-scale=1" />
@@ -45,8 +42,8 @@ export default function Meta({ title, keywords, description }) {
             <meta charSet="utf-8" />
             <link rel="icon" href={fevicon} />
             <title>{title}</title>
-            <meta type="copyright" content="Resume Maker" />
-            <meta type="author" content="HOT HEAD" />
+            <meta type="copyright" content="Reinhard Resume Maker" />
+            <meta type="author" content="Reinhard M. Babere" />
             {/* Open Graph */}
             <meta property="og:type" content="website" />
             <meta property="og:url" content={homepage} />
